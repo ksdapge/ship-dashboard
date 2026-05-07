f01c3dc3f2e0b10e5ec8e9aa0222a4feb0e6063e
Dumdumaya <KSDA@pge.com>
2026-05-07 11:25:06 -0700

    add edpi link

--- src/App.svelte
+++ src/App.svelte
@@ -258,6 +258,25 @@
   </div>
 </div>
 
+</div>
+
+<div>
+  <a 
+    href="https://edpi.cloud.pge.com/edpi/trend?did={ECF9A66B-568D-4B4A-82B5-3C447A75F852}" 
+    target="_blank" 
+    rel="noopener noreferrer"
+    style="color:#0089c4; text-decoration: underline; font-weight: bold;"
+  >
+    EDPI Link
+  </a>
+</div>
+
+<div style="margin-top: 1rem;">
+  <button on:click={calculateLoad}>
+    Calculate Load
+  </button>
+</div>
+
 <div style="margin-top: 1rem;">
   <button on:click={calculateLoad}>
     Calculate Load
